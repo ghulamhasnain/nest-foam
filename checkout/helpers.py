3f2dab1a9aa95587f76f17ea230555c49709057d
Ghulam Hasnain <ghulhasnain@hotmail.com>
2021-05-20 23:22:12 +0100

    checkout helpers

--- checkout/helpers.py
+++ checkout/helpers.py
@@ -9,6 +9,13 @@ def cart_count(request):
 def roundToTwoDecimalPlaces(number):
 	return float("{:.2f}".format(number))
 
+def roundtoTwoDecimalPlacesWithoutDecimal(number):
+	num = roundToTwoDecimalPlaces(number)
+	num = format(num, '.2f')
+	num = num.replace('.', '')
+	num = int(num)
+	return num
+
 def getCartContents(request):
 	try:
 		cart = request.session['cart']
@@ -29,6 +36,10 @@ def getCartTotal(request):
 	cart_total = 0
 	if cart_len > 0:
 		for c in cart:
-			cart_total = cart_total + c[-1]
+			cart_total = cart_total + c[-2]
 
 	return cart_total
+
+public_key = "pk_test_51InWoRCzModAlN49GTx8sTTobO6TIIX5Re6Uuadzq6QuyQaCFzxcKqQUnJqJpqvSJyKo37BQ4dzhLmGk1Fi8P8Jj00RevkBmaG"
+
+secret_key = "sk_test_51InWoRCzModAlN49mDMevVeGKNECHDDpQqKvOa62Y6d4PlYnOu3hipq688M3OvycoImgepdyMLRusujldK5FMwxK00YchhglyD"
